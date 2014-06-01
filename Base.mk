2a475a9ec815c43e660e2fd6648456e4b4d9db63
Christopher Moore <christopher.e.moore@gmail.com>
2014-05-31 18:30:28 -0700

    fixing up makefile ofr osx apps

--- Base.mk
+++ Base.mk
@@ -36,8 +36,12 @@ CFLAGS=-c -Wall -std=c++11
 CFLAGS_debug=-O0 -mfix-and-continue -gdwarf-2
 CFLAGS_release=-O3
 
-#LIBPATHS
+# old fashioned unix vars:
 #LIBS
+#LIBPATHS
+# newschool:
+#DYNAMIC_LIBS
+#STATIC_LIBS	<- haven't got this just yet
 
 LD=clang++
 LDFLAGS_lib=-dynamiclib -undefined suppress -flat_namespace
@@ -62,17 +66,78 @@ build_platform: $(PLATFORM)_debug $(PLATFORM)_release
 
 .PHONY: $(PLATFORM)_debug
 $(PLATFORM)_debug:
-	$(MAKE) BUILD="debug" builddist
+	$(MAKE) BUILD="debug" post_builddist_$(PLATFORM)
 
 .PHONY: $(PLATFORM)_release
 $(PLATFORM)_release:
-	$(MAKE) BUILD="release" builddist
+	$(MAKE) BUILD="release" post_builddist_$(PLATFORM)
 
 DISTDIR_BASE=dist
 DISTDIR=$(DISTDIR_BASE)/$(PLATFORM)/$(BUILD)
 DIST_PREFIX_osx_lib=lib
 DIST_SUFFIX_osx_lib=.dylib
-DIST=$(call concat,$(DISTDIR)/$(call buildVar,DIST_PREFIX)$(DIST_FILENAME)$(call buildVar,DIST_SUFFIX))
+DIST_SUFFIX_osx_app=.app/Contents/MacOS/$(DIST_FILENAME)
+DIST=$(DISTDIR)/$(call concat,$(call buildVar,DIST_PREFIX)$(DIST_FILENAME)$(call buildVar,DIST_SUFFIX))
+
+.PHONY: post_builddist_$(PLATFORM)
+post_builddist_$(PLATFORM): post_builddist_$(PLATFORM)_$(DIST_TYPE)
+
+.PHONY: post_builddist_$(PLATFORM)_$(DIST_TYPE)
+post_builddist_$(PLATFORM)_$(DIST_TYPE): builddist
+
+post_builddist_osx_app:
+	@printf APPLhect > $(dir $(DIST))../PkgInfo
+	@echo '<?xml version="1.0" encoding="UTF-8"?>' > $(dir $(DIST))../Info.plist
+	@echo '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' >> $(dir $(DIST))../Info.plist
+	@echo '<plist version="1.0">' >> $(dir $(DIST))../Info.plist
+	@echo '<dict>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundleName</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>$(DIST_FILENAME)</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundleIdentifier</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>net.christopheremoore.$(DIST_FILENAME)</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundleVersion</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>1.0</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundleIconFile</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>Icons</string>' >> $(dir $(DIST))../Info.plist
+	
+	@echo '	<key>CFBundleDevelopmentRegion</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>English</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundleDocumentTypes</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<array/>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundleExecutable</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>$(DIST_FILENAME)</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundleInfoDictionaryVersion</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>1.0</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundlePackageType</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>APPL</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>CFBundleSignature</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>hect</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>NSMainNibFile</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>MainMenu</string>' >> $(dir $(DIST))../Info.plist
+	@echo '	<key>NSPrincipalClass</key>' >> $(dir $(DIST))../Info.plist
+	@echo '	<string>NSApplication</string>' >> $(dir $(DIST))../Info.plist
+	
+	@echo '</dict>' >> $(dir $(DIST))../Info.plist
+	@echo '</plist>' >> $(dir $(DIST))../Info.plist
+	
+	-mkdir -p $(dir $(DIST))../Resources/lib
+	@for file in $(DYNAMIC_LIBS) $(call buildVar,DYNAMIC_LIBS); \
+	do \
+		cp $$file $(dir $(DIST))../Resources/lib; \
+		echo FIXING ../`basename $$file`/dist/$(PLATFORM)/$(BUILD)/`basename $$file`; \
+		install_name_tool \
+			-change \
+			dist/$(PLATFORM)/$(BUILD)/`basename $$file` \
+			\@executable_path/../Resources/lib/`basename $$file` \
+			$(DIST); \
+	done;
+	@echo done with $(DIST)
+	@echo
+
+# NOTICE the install_name_tool assumes the directory before dist matches the base name of the library
+#  it would be good to know how it is that install tool chooses the dir
+#  GLApp/test gives us 				dist/$(PLATFORM)/$(BUILD)/`basename $$file`
+# anything one root down gives us 	../$(SOMEDIR)/dist/$(PLATFORM)/$(BUILD)/`base $$file`
 
 .PHONY: builddist
 builddist: CFLAGS+= $(call buildVar,CFLAGS)
@@ -81,10 +146,8 @@ builddist: CFLAGS+= $(addprefix -D,$(MACROS) $(call buildVar,MACROS))
 builddist: LDFLAGS+= $(call buildVar,LDFLAGS)
 builddist: LDFLAGS+= $(addprefix -l,$(LIBS) $(call buildVar,LIBS))
 builddist: LDFLAGS+= $(addprefix -L,$(LIBPATHS) $(call buildVar,LIBPATHS))
+builddist: LDFLAGS+= $(DYNAMIC_LIBS)
 builddist: $(DIST)
-builddist: 
-	@echo done with $(DIST)
-	@echo
 
 $(OBJDIR)/%.o : $(SRCDIR_BASE)/%.cpp $(HEADERS)
 	-mkdir -p $(@D)
