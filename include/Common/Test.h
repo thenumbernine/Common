cfc1862fca02ac671b1813961076a6ef759b4954
Christopher Moore <christopher.e.moore@gmail.com>
2014-06-16 00:25:16 -0700

    missed a parenthsis wrapper in a macro variable

--- include/Common/Test.h
+++ include/Common/Test.h
@@ -6,4 +6,4 @@
 
 #define COMMA	,
 #define ECHO(x)	std::cout << #x << "\t" << (x) << std::endl;
-#define TEST_EQ(a,b) std::cout << #a << " == " << #b << " :: " << (a) << " == " << (b) << std::endl; if (a != b) throw Common::Exception() << "failed";
+#define TEST_EQ(a,b) std::cout << #a << " == " << #b << " :: " << (a) << " == " << (b) << std::endl; if ((a) != (b)) throw Common::Exception() << "failed";
