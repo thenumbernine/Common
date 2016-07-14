559666af46e6f724e581e36a202a19dd6ac4c64d
Christopher Moore <christopher.e.moore@gmail.com>
2016-07-14 04:51:12 +0300

    added endof macro

--- include/Common/Macros.h
+++ include/Common/Macros.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define numberof(x)	(sizeof(x)/sizeof((x)[0]))
+#define endof(x) ((x) + numberof(x))
 #define frand() ((double)rand() / (double)RAND_MAX)
 #define crand()	(frand() * 2. - 1.)
 
