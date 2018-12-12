470c4c72bdb21ffa1795b2c614317af5abfd8066
Christopher Moore <christopher.e.moore@gmail.com>
2018-12-13 00:40:59 +0100

    some minor changes

--- include/Common/gl.h
+++ include/Common/gl.h
@@ -23,7 +23,7 @@ I would've put it in GLApp, but then projects that use GL but not GLApp would st
 
 #include <windows.h>
 #define GL_GLEXT_PROTOTYPES
-#include "GL/glew.h"
+//#include "GL/glew.h"
 #include <GL/gl.h>
 //#include "GL/glext.h"
 
