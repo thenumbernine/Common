8049129c3cd910c720b677136d1ae4bd04e793fb
Christopher Moore <christopher.e.moore@gmail.com>
2023-01-09 16:29:46 +0700

    moved these from Profiler to Common

--- include/Common/Macros.h
+++ include/Common/Macros.h
@@ -5,6 +5,11 @@
 #define frand() ((double)rand() / (double)RAND_MAX)
 #define crand()	(frand() * 2. - 1.)
 
+//http://stackoverflow.com/questions/3859944/combining-string-literals-and-integer-constants
+#define STRINGIZE_DETAIL_(v)	#v
+#define STRINGIZE(v) STRINGIZE_DETAIL_(v)
+#define LINE_STRING	STRINGIZE(__LINE__)
+
 //I need a unique api macro for *each project*
 //otherwise how can I determine which class apis are import and which are export ...
 //here's how a single library does it:
