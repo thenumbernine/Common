4392c61198e19fd32496715e6c0d6b9b9e5dcbc8
Christopher Moore <christopher.e.moore@gmail.com>
2024-02-26 15:22:42 +0700

    added readAsVector

--- include/Common/File.h
+++ include/Common/File.h
@@ -3,15 +3,48 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include "Exception.h"
 
 namespace Common {
+namespace File {
+	
+	//I would like return template specializatoin ...
+	//template<typename Result = std::string>
+	//static Result read(std::string const & filename);
+	//inline std::string read<std::string>(std::string const & filename) {
+	// but instead I'm stuck with ...
+	inline std::string read(std::string const & filename) {
+		std::ifstream f(filename);
+		if (!f.good()) throw Common::Exception() << "failed to open file " << filename;
+		f.seekg(0, f.end);
+		size_t len = f.tellg();
+		f.seekg(0, f.beg);
+		
+		std::vector<char> buf(len);
+		
+		f.read(&buf[0], len);
+		
+		return std::string(buf.begin(), buf.end());
+	}
 
-struct File {
-	static std::string read(const std::string& filename);
-	static void write(const std::string& filename, const std::string& data);
-	static std::string getExtension(const std::string& filename);
-	static bool exists(const std::string& filename);
-	static void remove(const std::string& filename);
-};
-
+	template<typename T>
+	inline std::vector<T> readAsVector(std::string const & filename) {
+		std::ifstream f(filename);
+		if (!f.good()) throw Common::Exception() << "failed to open file " << filename;
+		f.seekg(0, f.end);
+		size_t len = f.tellg();
+		f.seekg(0, f.beg);
+		
+		std::vector<T> result(len);
+		
+		f.read(result.data(), len);
+		
+		return result;
+	}
+	
+	void write(const std::string& filename, const std::string& data);
+	std::string getExtension(const std::string& filename);
+	bool exists(const std::string& filename);
+	void remove(const std::string& filename);
+}
 }
--- src/File.cpp
+++ src/File.cpp
@@ -3,34 +3,25 @@
 #include <fstream>
 
 namespace Common {
+namespace File {
 
-std::string File::read(const std::string& filename) {
-	std::ifstream f(filename);
-	if (!f.good()) throw Common::Exception() << "failed to open file " << filename;
-	f.seekg(0, f.end);
-	size_t len = f.tellg();
-	f.seekg(0, f.beg);
-	std::vector<char> buf(len);
-	f.read(&buf[0], len);
-	return std::string(buf.begin(), buf.end());
-}
-
-void File::write(const std::string& filename, const std::string& data) {
+void write(const std::string& filename, const std::string& data) {
 	std::ofstream(filename) << data;
 }
 
-std::string File::getExtension(const std::string& filename) {
+std::string getExtension(const std::string& filename) {
 	std::string::size_type i = filename.rfind('.');
 	if (i == std::string::npos) return filename;
 	return filename.substr(i+1);
 }
 
-bool File::exists(const std::string& filename) {
+bool exists(const std::string& filename) {
 	return std::ifstream(filename).good();
 }
 
-void File::remove(const std::string& filename) {
+void remove(const std::string& filename) {
 	std::remove(filename.c_str());
 }
 
+}
 }
